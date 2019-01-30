013587c88136adbe458d49ddef2efec240f7e334
Jim Heising <jheising@gmail.com>
2019-01-29 17:30:41 -0800

    Bug fix

--- lib/Services/KeyValue/Providers/DiskKVService.ts
+++ lib/Services/KeyValue/Providers/DiskKVService.ts
@@ -19,7 +19,7 @@ export class DiskKVService extends KVServiceProvider {
         return service;
     }
 
-    setValue(namespace: string, key: string, value: any, expirationInSeconds: number):Promise<void> {
+    setValue(namespace: string, key: string, value: any, expirationInSeconds: number): Promise<void> {
 
         let data = {
             value: value,
@@ -29,29 +29,28 @@ export class DiskKVService extends KVServiceProvider {
         return DiskKVService._fileService.writeFile(`${APIUtils.slugify(namespace)}/${APIUtils.slugify(key)}.json`, JSON.stringify(data));
     }
 
-    hasValue(namespace: string, key: string):Promise<boolean> {
+    hasValue(namespace: string, key: string): Promise<boolean> {
         return DiskKVService._fileService.pathExists(`${APIUtils.slugify(namespace)}/${APIUtils.slugify(key)}.json`);
     }
 
-    private _getValue(namespace: string, key: string):Promise<{value:any, expires:number}> {
+    private _getValue(namespace: string, key: string): Promise<{ value: any, expires: number }> {
         return DiskKVService._fileService.readFile(`${APIUtils.slugify(namespace)}/${APIUtils.slugify(key)}.json`).then((fileContents) => {
             let data = JSON.parse(fileContents);
             return Promise.resolve(data);
         }).catch(() => {
+            // Ignore errors
             return;
         });
     }
 
-    getValue(namespace: string, key: string):Promise<any> {
+    getValue(namespace: string, key: string): Promise<any> {
         return this._getValue(namespace, key).then((data) => {
 
-            if(isNil(data))
-            {
+            if (isNil(data)) {
                 return Promise.resolve();
             }
 
-            if(data.expires && data.expires <= Date.now())
-            {
+            if (data.expires && data.expires <= Date.now()) {
                 return this.deleteValue(namespace, key).then(() => Promise.resolve(data.value));
             }
 
@@ -59,21 +58,17 @@ export class DiskKVService extends KVServiceProvider {
         });
     }
 
-    deleteValue(namespace: string, key: string):Promise<void> {
-        try {
-            return DiskKVService._fileService.deleteFile(`${APIUtils.slugify(namespace)}/${APIUtils.slugify(key)}.json`);
-        }
-        // Ignore any errors
-        catch (e) {
-            return Promise.resolve();
-        }
+    deleteValue(namespace: string, key: string): Promise<void> {
+        return DiskKVService._fileService.deleteFile(`${APIUtils.slugify(namespace)}/${APIUtils.slugify(key)}.json`).catch((e) => {
+            // Ignore errors
+            return;
+        });
     }
 
-    updateExpiration(namespace: string, key: string, expirationInSeconds: number):Promise<void> {
+    updateExpiration(namespace: string, key: string, expirationInSeconds: number): Promise<void> {
         return this._getValue(namespace, key).then((data) => {
 
-            if(isNil(data))
-            {
+            if (isNil(data)) {
                 return Promise.resolve();
             }
 
